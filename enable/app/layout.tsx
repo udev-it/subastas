13ab5b66ca39883d410b406e970ae578b2e2b0be
MiguelMJH <mj371440@gmail.com>
2025-06-26 18:32:47 -0600

    Agrego carpeta de HU(enable), proyecto independiente del sistema

---
+++ enable/app/layout.tsx
@@ -0,0 +1,29 @@
+import type React from "react"
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Supabase Bidding Interface",
+  description: "Interface for managing users and making bids through Supabase",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
